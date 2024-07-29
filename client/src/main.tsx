d1a428d04abad7c50711a2e282015b7d1d9b4974
Lazare Rossillon <lazrossi@student.42.fr>
2024-07-29 09:47:50 +0200

    3 changes to get working on sub-url of Baas LP

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,7 +9,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/transcript-seeker">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
