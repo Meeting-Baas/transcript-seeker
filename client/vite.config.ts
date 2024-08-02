8934ded94078b9675152b10c87a536d06cda883f
Lazare Rossillon <lazrossi@student.42.fr>
2024-08-02 17:26:14 +0200

    format

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,63 +5,63 @@ import { defineConfig, loadEnv } from 'vite';
 import checker from 'vite-plugin-checker';
 
 export default defineConfig(({ mode }) => {
-    dotenv.config({ path: resolve(__dirname, './.env') });
-    const env = loadEnv(mode, path.resolve(__dirname, './'));
+  dotenv.config({ path: resolve(__dirname, './.env') });
+  const env = loadEnv(mode, path.resolve(__dirname, './'));
 
-    const MEETINGBASS_API_URL = env.VITE_MEETINGBASS_API_URL;
-    const MEETINGBASS_S3_URL = env.VITE_MEETINGBASS_S3_URL;
-    const VITE_SERVER_API_URL = env.VITE_SERVER_API_URL;
-    const VITE_BAAS_PROXY_URL = env.VITE_BAAS_PROXY_URL;
-    const VITE_S3_PROXY_URL = env.VITE_S3_PROXY_URL;
-    const HOST = env.HOST;
-    const PORT = env.PORT;
+  const MEETINGBASS_API_URL = env.VITE_MEETINGBASS_API_URL;
+  const MEETINGBASS_S3_URL = env.VITE_MEETINGBASS_S3_URL;
+  const VITE_SERVER_API_URL = env.VITE_SERVER_API_URL;
+  const VITE_BAAS_PROXY_URL = env.VITE_BAAS_PROXY_URL;
+  const VITE_S3_PROXY_URL = env.VITE_S3_PROXY_URL;
+  const HOST = env.HOST;
+  const PORT = env.PORT;
 
-    return {
-        base: '/transcript-seeker/', // use relative paths
-        plugins: [
-            react(),
-            checker({
-                typescript: true,
-            }),
-        ],
-        server: {
-            proxy: {
-                [`${VITE_SERVER_API_URL}`]: {
-                    target: `http://${HOST}:${PORT}`,
-                    changeOrigin: true,
-                    secure: false,
-                    ws: true,
-                    configure: (proxy, _options) => {
-                        proxy.on('error', (err, _req, _res) => {
-                            console.log('proxy error', err);
-                        });
-                        proxy.on('proxyReq', (proxyReq, req, _res) => {
-                            console.log('Sending Request to the Target:', req.method, req.url);
-                        });
-                        proxy.on('proxyRes', (proxyRes, req, _res) => {
-                            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-                        });
-                    },
-                },
-                [`${VITE_BAAS_PROXY_URL}`]: {
-                    target: MEETINGBASS_API_URL,
-                    changeOrigin: true,
-                    rewrite: (path) => path.replace(new RegExp(`^${VITE_BAAS_PROXY_URL}`), ''),
-                    secure: true,
-                },
-                [`${VITE_S3_PROXY_URL}`]: {
-                    target: MEETINGBASS_S3_URL,
-                    changeOrigin: true,
-                    rewrite: (path) => path.replace(new RegExp(`^${VITE_S3_PROXY_URL}`), ''),
-                    secure: true,
-                },
-            },
+  return {
+    base: '/transcript-seeker/', // use relative paths
+    plugins: [
+      react(),
+      checker({
+        typescript: true,
+      }),
+    ],
+    server: {
+      proxy: {
+        [`${VITE_SERVER_API_URL}`]: {
+          target: `http://${HOST}:${PORT}`,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          configure: (proxy, _options) => {
+            proxy.on('error', (err, _req, _res) => {
+              console.log('proxy error', err);
+            });
+            proxy.on('proxyReq', (proxyReq, req, _res) => {
+              console.log('Sending Request to the Target:', req.method, req.url);
+            });
+            proxy.on('proxyRes', (proxyRes, req, _res) => {
+              console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+            });
+          },
         },
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './src'),
-                '@meeting-baas/shared': path.resolve(__dirname, '../packages/shared/src'),
-            },
+        [`${VITE_BAAS_PROXY_URL}`]: {
+          target: MEETINGBASS_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(new RegExp(`^${VITE_BAAS_PROXY_URL}`), ''),
+          secure: true,
         },
-    };
+        [`${VITE_S3_PROXY_URL}`]: {
+          target: MEETINGBASS_S3_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(new RegExp(`^${VITE_S3_PROXY_URL}`), ''),
+          secure: true,
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@meeting-baas/shared': path.resolve(__dirname, '../packages/shared/src'),
+      },
+    },
+  };
 });
