5a311d048e732564de14ac5fc125f17fc4fa2836
Lazare Rossillon <lazrossi@student.42.fr>
2024-08-03 10:06:24 +0200

    revert /root layout to previous, clearer layout.
    Also we now check if AssemblyAI key exists

--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -2,12 +2,16 @@ import ServerAlert from '@/components/server-alert';
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { useApiKeysStore, useServerAvailabilityStore } from '@/store';
+
 import { SettingsIcon } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 function RootPage() {
   const serverAvailability = useServerAvailabilityStore((state) => state.serverAvailability);
   const baasApiKey = useApiKeysStore((state) => state.baasApiKey);
+  const gladiaApiKey = useApiKeysStore((state) => state.gladiaApiKey);
+  const assemblyAiKey = useApiKeysStore((state) => state.assemblyAIApiKey);
+  const apiKeysExist = baasApiKey || gladiaApiKey || assemblyAiKey;
 
   return (
     <div className="relative flex h-full min-h-[calc(100dvh-94px)] flex-col items-center justify-center space-y-2 px-4">
@@ -20,28 +24,45 @@ function RootPage() {
       <div className="flex w-full max-w-md flex-col items-center justify-center">
         <h1 className="mb-2 text-center text-4xl font-bold">Transcript Seeker</h1>
         <p className="mb-6 text-center text-lg">
-          Open-source interface by{' '}
           <a
-            href="https://github.com/meeting-baas/meeting-bot-as-a-service/apps/"
+            href="https://github.com/meeting-baas/transcript-seeker"
             target="_blank"
             rel="noopener noreferrer"
             className="text-blue-600 hover:underline"
           >
-            Meeting Baas 🐟
-          </a>
+            Open-source
+          </a>{' '}
+          interface by{' '}
+          <a
+            href="https://meetingbaas.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 hover:underline"
+          >
+            Meeting Baas
+          </a>{' '}
+          🐟
         </p>
         <div className="flex w-full items-center gap-4 pb-2">
-          <Link to="/settings" className={cn(buttonVariants({ variant: 'secondary' }), 'flex-1')}>
-            <SettingsIcon className="mr-2 h-4 w-4" /> Settings
-          </Link>
           <Link to="/meetings" className={cn(buttonVariants({ variant: 'outline' }), 'flex-1')}>
             Recordings
           </Link>
         </div>
-        <div className="flex w-full gap-4">
+        <div className="flex w-full flex-col gap-2 sm:flex-row sm:gap-4">
+          <Link
+            to="/settings"
+            className={cn(
+              buttonVariants({ variant: apiKeysExist ? 'secondary' : 'default' }),
+              'h-16 flex-1 text-lg',
+            )}
+          >
+            <SettingsIcon className="mr-2 h-4 w-4" /> API Keys
+          </Link>
           <Link
             to="/upload"
-            className={cn(buttonVariants({ variant: 'default' }), 'h-16 flex-1 text-lg')}
+            className={cn(buttonVariants({ variant: 'default' }), 'h-16 flex-1 text-lg', {
+              'pointer-events-none opacity-50': !gladiaApiKey && !assemblyAiKey,
+            })}
           >
             Upload File
           </Link>
