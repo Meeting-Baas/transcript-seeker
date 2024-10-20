7cdd69ad059c3a56e1be6bfb0d5d70dd93f38e1d
Anirudh Sriram <hello@techwithanirudh.com>
2024-10-20 09:19:36 +0000

    fix: fonts

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+// import { fontFamily } from "tailwindcss/defaultTheme";
 
 import baseConfig from "@meeting-baas/tailwind-config/web";
 
@@ -9,11 +9,11 @@ export default {
   content: [...baseConfig.content, "../../packages/ui/src/*.{ts,tsx}"],
   presets: [baseConfig],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-        mono: ["var(--font-geist-mono)", ...fontFamily.mono],
-      },
-    },
+    // extend: {
+    //   fontFamily: {
+    //     sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+    //     mono: ["var(--font-geist-mono)", ...fontFamily.mono],
+    //   },
+    // },
   },
 } satisfies Config;
