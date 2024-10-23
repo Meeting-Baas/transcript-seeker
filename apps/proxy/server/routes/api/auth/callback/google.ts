7a02e3aa0b48a16fd1d215c20ec7b913ea423e65
Anirudh Sriram <hello@techwithanirudh.com>
2024-10-23 13:26:12 +0000

    fix: eror handling

--- apps/proxy/server/routes/api/auth/callback/google.ts
+++ apps/proxy/server/routes/api/auth/callback/google.ts
@@ -8,7 +8,7 @@ export default defineEventHandler(async (event) => {
     useRuntimeConfig(event).googleRedirectUri,
   );
 
-  if (!query.code) return { error: 'Failed to authorize' };
+  if (!query.code) return { error: 'Authorization failed: No authorization code received from Google' };
 
   const { code } = query;
 
@@ -16,8 +16,15 @@ export default defineEventHandler(async (event) => {
     const response = await oAuth2Client.getToken(code.toString());
     oAuth2Client.setCredentials(response.tokens);
 
-    return { tokens: response.tokens };
-  } catch {
-    return { error: 'Failed to authorize.' };
+    return { data: response.tokens };
+  } catch (err) {
+    if (err instanceof Error) {
+      // todo: check if it is safe to expose this data (it is but just in case)
+      console.error('Authorization error:', err.message);
+      return { error: `Failed to authorize: ${err.message}` };
+    } else {
+      console.error('Unknown authorization error:', err);
+      return { error: 'Failed to authorize due to an unknown error.' };
+    }
   }
 });
