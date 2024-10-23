a7f0388772fbaeb346d314465cda8e82eb8be8ff
Anirudh Sriram <hello@techwithanirudh.com>
2024-10-23 02:57:48 +0000

    remove: pwa

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,68 +1,8 @@
 import path from 'path';
-import type { VitePWAOptions } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import replace from '@rollup/plugin-replace';
 import react from '@vitejs/plugin-react-swc';
 import checker from 'vite-plugin-checker';
-import { VitePWA } from 'vite-plugin-pwa';
-
-const pwaOptions: Partial<VitePWAOptions> = {
-  mode: 'development',
-  base: '/',
-  includeAssets: ['logo.svg'],
-  registerType: 'autoUpdate',
-  manifest: {
-    name: 'Transcript Seeker',
-    short_name: 'Transcript Seeker',
-    theme_color: '#78FFF0',
-    icons: [
-      {
-        src: 'pwa-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/pwa-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-      {
-        src: 'pwa-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable',
-      },
-    ],
-  },
-  devOptions: {
-    enabled: process.env.SW_DEV === 'true',
-    /* when using generateSW the PWA plugin will switch to classic */
-    type: 'module',
-    navigateFallback: 'index.html',
-  },
-};
-
-const replaceOptions = { __DATE__: new Date().toISOString() };
-const reload = process.env.RELOAD_SW === 'true';
-const selfDestroying = process.env.SW_DESTROY === 'true';
-
-if (process.env.SW === 'true') {
-  pwaOptions.srcDir = 'src';
-  pwaOptions.filename = 'service-worker.ts';
-  pwaOptions.strategies = 'injectManifest';
-  pwaOptions.injectManifest = {
-    minify: false,
-    enableWorkboxModulesLogs: true,
-    maximumFileSizeToCacheInBytes: 11000000,
-  };
-}
-
-if (reload) {
-  // @ts-expect-error just ignore
-  replaceOptions.__RELOAD_SW__ = 'true';
-}
-
-if (selfDestroying) pwaOptions.selfDestroying = selfDestroying;
 
 // @ts-ignore
 export default defineConfig(() => {
@@ -75,9 +15,7 @@ export default defineConfig(() => {
       react(),
       checker({
         typescript: true,
-      }),
-      VitePWA(pwaOptions),
-      replace({ preventAssignment: true, values: replaceOptions }),
+      })
     ],
     server: {
       port: CLIENT_PORT,
