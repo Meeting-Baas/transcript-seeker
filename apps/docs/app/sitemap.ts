ea162f22201c3416622049c8eb6a6f50db8a303d
Anirudh Sriram <hello@techwithanirudh.com>
2024-10-30 13:23:24 +0000

    fix:

--- apps/docs/app/sitemap.ts
+++ apps/docs/app/sitemap.ts
@@ -25,7 +25,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     },
     ...(await Promise.all(
       source.getPages().map(async (page) => {
-        const { lastModified } = await page.data.load();
+        const { lastModified } = page.data;
         return {
           url: url(page.url),
           lastModified: lastModified ? new Date(lastModified) : undefined,
