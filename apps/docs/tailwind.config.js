99624427c1cad843da1210fbe9bfa7be0a90c8ce
Anirudh Sriram <hello@techwithanirudh.com>
2024-11-01 10:59:21 +0000

    fix

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -1,4 +1,4 @@
-import { createPreset } from "fumadocs-ui/tailwind-plugin";
+import { createPreset, presets } from "fumadocs-ui/tailwind-plugin";
 import baseConfig from "@meeting-baas/tailwind-config/web";
 
 /** @type {import('tailwindcss').Config} */
@@ -14,8 +14,22 @@ export default {
   presets: [
     createPreset({
       addGlobalColors: true,
-      preset: "vitepress",
+      preset: {
+        ...presets.default,
+        dark: {
+          ...presets.default.dark,
+          background: '0 0% 17%',
+          foreground: ' 0 0% 100%',
+          popover: '0 0% 17%',
+          card: '0 0% 17%',
+          muted: '173 8% 22%',
+          border: '173 8% 22%',
+          accent: '173 8% 22%',
+          'accent-foreground': '0 0% 100%',
+          'muted-foreground': '0 0% 65%',
+        },
+      }
     }),
   ],
-  plugins: [baseConfig.plugins, require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate")],
 };
