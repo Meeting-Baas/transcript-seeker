907af887329dda668f84023dd9e6ee21d0304112
Anirudh Sriram <hello@techwithanirudh.com>
2024-11-01 11:02:40 +0000

    fix: match baas colors

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -17,16 +17,22 @@ export default {
       preset: {
         ...presets.default,
         dark: {
-          ...presets.default.dark,
           background: '0 0% 17%',
-          foreground: ' 0 0% 100%',
-          popover: '0 0% 17%',
+          foreground: '0 0% 100%',
           card: '0 0% 17%',
+          "card-foreground": "0 0% 100%",
+          popover: '0 0% 17%',
+          "popover-foreground": "0 0% 100%",
+          primary: "176 100% 43%",
+          "primary-foreground": "213 16% 27%",
+          secondary: "173 8% 22%",
+          "secondary-foreground": "0 0% 100%",
           muted: '173 8% 22%',
+          "muted-foreground": "0 0% 65%",
           border: '173 8% 22%',
           accent: '173 8% 22%',
           'accent-foreground': '0 0% 100%',
-          'muted-foreground': '0 0% 65%',
+          ring: "197 100% 44%"
         },
       }
     }),
