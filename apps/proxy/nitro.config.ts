8a086db575849f3ffd3dba838beae0b3aa3f818e
Anirudh Sriram <hello@techwithanirudh.com>
2024-11-06 04:34:53 +0000

    fix: baas-url

--- apps/proxy/nitro.config.ts
+++ apps/proxy/nitro.config.ts
@@ -3,7 +3,7 @@ import process from 'node:process';
 
 const env = process.env;
 
-const MEETINGBAAS_API_URL = env.NITRO_MEETINGBAAS_API_URL || 'https://api.meeting-baas.com';
+const MEETINGBAAS_API_URL = env.NITRO_MEETINGBAAS_API_URL || 'https://api.meetingbaas.com';
 const MEETINGBAAS_S3_URL =
   env.NITRO_MEETINGBAAS_S3_URL || 'https://s3.eu-west-3.amazonaws.com/meeting-baas-video';
 
